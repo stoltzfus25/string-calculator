4f955cf0ac1187d345073592fafb11b509b8fdc2
Samuel Stoltzfus <samuel@superiorplastic.net>
2019-09-10 10:52:11 -0600

    initial Xcode project

---
+++ StringCalculator/StringCalculator.h
@@ -0,0 +1,19 @@
+//
+//  StringCalculator.h
+//  StringCalculator
+//
+//  Created by Samuel Stoltzfus on 9/10/19.
+//  Copyright © 2019 Superior Plastic Products, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StringCalculator.
+FOUNDATION_EXPORT double StringCalculatorVersionNumber;
+
+//! Project version string for StringCalculator.
+FOUNDATION_EXPORT const unsigned char StringCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringCalculator/PublicHeader.h>
+
+
