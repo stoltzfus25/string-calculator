988ad828b0701322c59a100c922d38efc548614f
stoltzfus25 <samuel@mikesclan.com>
2019-09-10 11:00:00 -0600

    set to starting point

---
+++ StringCalculator/StringCalculator.swift
@@ -0,0 +1,9 @@
+//
+//  StringCalculator.swift
+//  StringCalculator
+//
+//  Created by Samuel Stoltzfus on 9/10/19.
+//  Copyright © 2019 Superior Plastic Products, Inc. All rights reserved.
+//
+
+import Foundation
